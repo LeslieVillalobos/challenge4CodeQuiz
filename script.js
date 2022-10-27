0aff1c004b6ce65eb0a051fd4ab1555c256adb78
wolfvilla <lvillalobos258@gmail.com>
2022-10-27 10:43:11 -0500

    adding changes

---
+++ script.js
@@ -0,0 +1,87 @@
+// var button = document.querySelector(".button");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let question = {
+//     title: 'Commonly used data types DO NOT iclude', 
+//     alternatives: ['strings', 'booleans', 'alerts', 'numbers'],
+//     correctAnswer: alerts
+// };
+
+// let question= {
+//     title: 'Commonly used data types DO NOT iclude', 
+//     alternatives: ['strings', 'booleans', 'alerts', 'numbers'],
+//     correctAnswer: alerts
+// };
+
+// let question = {
+//     title: 'Commonly used data types DO NOT iclude', 
+//     alternatives: ['strings', 'booleans', 'alerts', 'numbers'],
+//     correctAnswer: alerts
+// };
+
+// let question = {
+//     title: 'Commonly used data types DO NOT iclude', 
+//     alternatives: ['strings', 'booleans', 'alerts', 'numbers'],
+//     correctAnswer: alerts
+// };
+// function showQuestion(q) {
+
+//     // 1. select dom element
+//     let titleDiv = document.getElementById('title');
+
+//     // 2. modify it
+//     titleDiv.textContent = question.title;
+
+//     // selecting by a query
+//     let alts = document.querySelectorAll('.alternative');
+
+
+//     alts.forEach(function(element, index){
+//         element.textContent = q.alternatives[index];
+
+//         element.addEventListener('click', function(){
+//             // check correct answer
+//             if(q.correctAnswer == index) {
+//                 console.log('Correct Answer!');
+//             }
+//             else {
+//                 console.log('Wrong Answer!');
+//             }
+//         });
+//     });
+// }
+
+// showQuestion(question);
+
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function(){
+//     console.log('clicked');
+// });
